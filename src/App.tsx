8370dbdfd63fbd42bef8add93bec75ff65c351c3
“Sid <sid@devopsdirective.com>
2026-03-13 20:46:00 -0400

    Move guides pages behind shared header

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Guides from "./pages/Guides";
-import BuildingOpenLovablePart1 from "./pages/BuildingOpenLovablePart1";
+import Guides from "./pages/guides/Guides";
+import BuildingOpenLovablePart1 from "./pages/guides/BuildingOpenLovablePart1";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
